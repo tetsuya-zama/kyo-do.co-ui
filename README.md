30280d21da77a9257d97c77f0762089c9c03bdaa
tetsuya.zama <tetsuya.zama@gmail.com>
2017-02-24 00:24:48 +0900

    npmとwebpack周りの初期設定追加

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 行き先掲示板サービス　"ドコイッテル？"のUIです。
+## 環境構築
+### 初期セットアップ
+```bash
+$ git clone https://github.com/tetsuya-zama/dokoit.tel-ui.git
+$ cd dokoit.tel-ui
+$ npm install
+```
+### ビルド
+```bash
+$ webpack
+```
+
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
