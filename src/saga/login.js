7cc933b5d3e2d233dce01e524a6cc1274103984e
tetsuya.zama <tetsuya.zama@gmail.com>
2017-02-25 17:28:36 +0900

    ID/Passの記憶機能(いわゆるRemember Me)

--- src/saga/login.js
+++ src/saga/login.js
@@ -1,5 +1,12 @@
 import {take,put,takeEvery} from 'redux-saga/effects'
-import {LOGIN_REQUESTED,loginSuccess,loginFailure} from '../action/login'
+import {LOGIN_REQUESTED,LOGIN_FAILURE,LOGOUT_REQUESTED,loginRequested,loginSuccess,loginFailure} from '../action/login'
+import {getFromStorage,setToStorage,removeFromStorage,existsKeyOnStorage} from '../module/localstorage'
+
+/**
+* rememberme情報（入力されたID/Pass）をlocalStorageに保存する際のkey
+*/
+const REMEMBER_ME_STORAGE_KEY = "rememberme";
+
 /**
 * login要求を受け付けるSaga
 * @see http://qiita.com/kuy/items/716affc808ebb3e1e8ac
@@ -21,6 +28,10 @@ function* loginTask(action){
   * TODO ダミーロジック。実際にはAPIにアクセスしてログイン成否をハンドリングする必要あり。
   * ここではtest/testのみログイン可能とする。
   */
+
+  //rememberme
+  setToStorage(REMEMBER_ME_STORAGE_KEY,{id:action.payload.id,pass:action.payload.pass});
+
   if(action.payload.id == "test" && action.payload.pass == "test"){
     const dummyUserInfo = {userid:"test", token:"dummy"};
     //LOGIN_SUCCESSアクションをput(dispatch)
@@ -28,5 +39,39 @@ function* loginTask(action){
   }else{
     //LOGIN_FAILUREアクションをput(dispatch)
     yield put(loginFailure());
+  }
+}
+/**
+* logout要求を受けるSaga
+* @see http://qiita.com/kuy/items/716affc808ebb3e1e8ac
+*/
+export function* logoutSaga(){
+  yield takeEvery(LOGOUT_REQUESTED,cleanRememberMeTask);
+}
+
+/**
+* ログインに失敗したときのクリーンアップを行うSaga
+* @see http://qiita.com/kuy/items/716affc808ebb3e1e8ac
+*/
+export function* loginFailureSaga(){
+  yield takeEvery(LOGIN_FAILURE,cleanRememberMeTask);
+}
+/**
+* localStorageに保存されているrememberme情報をクリアするTask
+*/
+function* cleanRememberMeTask(){
+  yield removeFromStorage(REMEMBER_ME_STORAGE_KEY);
+}
+
+/**
+* localStorageに保存されているID/PassからログインするSaga
+* アプリを起動して(ブラウザアクセスして)1回だけ実行される
+* @see http://qiita.com/kuy/items/716affc808ebb3e1e8ac
+*/
+export function* loginFromRememberMeSaga(){
+  const remembermeSaved = yield existsKeyOnStorage(REMEMBER_ME_STORAGE_KEY);
+  if(remembermeSaved){
+    const rememberme = yield getFromStorage(REMEMBER_ME_STORAGE_KEY);
+    yield put(loginRequested(rememberme.id,rememberme.pass));
   }
 }
--- src/saga/root.js
+++ src/saga/root.js
@@ -1,5 +1,5 @@
 import {fork} from 'redux-saga/effects'
-import {loginSaga} from './login'
+import {loginSaga,logoutSaga,loginFailureSaga,loginFromRememberMeSaga} from './login'
 import {loadDestinationSaga,changeDestinationSaga} from './mydestination'
 
 /**
@@ -8,6 +8,9 @@ import {loadDestinationSaga,changeDestinationSaga} from './mydestination'
 */
 export default function* rootSaga(){
   yield fork(loginSaga);
+  yield fork(logoutSaga);
+  yield fork(loginFailureSaga);
+  yield fork(loginFromRememberMeSaga);
   yield fork(loadDestinationSaga);
   yield fork(changeDestinationSaga);
 }
