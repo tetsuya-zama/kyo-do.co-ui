0c2bfb1fd2c559ad2c8f055e15de786e60ab4eb2
tetsuya.zama <tetsuya.zama@gmail.com>
2017-03-03 00:29:42 +0900

    LoginとSignUpをモックからAPIを叩く実装に変更

--- src/saga/signup.js
+++ src/saga/signup.js
@@ -2,6 +2,7 @@ import {put,call,takeEvery} from 'redux-saga/effects'
 import {SIGNUP_REQUIRED,signupSucceess,signupFailure} from '../action/signup'
 import {SIGNUP_FAILURE_REASONS} from '../const/signup'
 import {loginRequested} from '../action/login'
+import axios from "axios";
 
 //MOCK用
 //TODO 実装終わったら消す
@@ -21,53 +22,16 @@ export function* signupSaga(){
 * @param {Object} action SIGNUP_REQUIREDアクション
 */
 function* signupTask(action){
+  try{
+    const result = yield call(axios.post,"https://api.kyo-do.co/user",{
+      userid:action.payload.id,
+      password:action.payload.password,
+      name:action.payload.name});
 
-  //MOCK
-  //本来はAPIを叩いて確認する
-  //TODO 本実装
-  const idDuplicated = isIDDuplicate(action.payload.id);
-  if(idDuplicated){
+    yield put(signupSucceess());
+    yield put(loginRequested(action.payload.id,action.payload.password));
+  }catch(e){
+    //XXX　エラーが起きればID重複と判断しているが、サーバエラーと区別したい
     yield put(signupFailure([SIGNUP_FAILURE_REASONS.ID_DUPLICATED]));
-  }else{
-    try{
-      //MOCK
-      //本来はAPIを叩いて登録する
-      //TODO 本実装
-      yield registerUser(action.payload);
-      yield put(signupSucceess());
-      yield put(loginRequested(action.payload.id,action.payload.password));
-    }catch(e){
-      yield put(SIGNUP_FAILURE[SIGNUP_FAILURE_REASONS.SERVER_ERROR]);
-    }
-  }
-}
-
-//以下の関数はMOCK用
-//TODO 実装終わったら消す
-
-/**
-* IDが重複しているかどうか確認する
-* @param {string} id 確認するID
-* @return {boolean} 重複していればTrue
-*/
-function isIDDuplicate(id){
-  if(!existsKeyOnStorage(MOCK_MEMBER_REPO_KEY)){
-    return false;
-  }else{
-    const repo = getFromStorage(MOCK_MEMBER_REPO_KEY);
-    return repo.filter(m => m.id == id).length > 0;
-  }
-}
-/**
-* ユーザーをMOCKレポジトリに登録する
-* @param {{id:string,password:string,name:string}} userinfo 登録を要求するユーザー情報
-* @return {undefined}
-*/
-function registerUser(userinfo){
-  if(!existsKeyOnStorage(MOCK_MEMBER_REPO_KEY)){
-    setToStorage(MOCK_MEMBER_REPO_KEY,[userinfo]);
-  }else{
-    const repo = getFromStorage(MOCK_MEMBER_REPO_KEY);
-    setToStorage(MOCK_MEMBER_REPO_KEY, repo.concat([userinfo]));
   }
 }
