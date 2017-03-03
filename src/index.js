e09216d209898582113c33c5ab290b23e986993a
tetsuya.zama <tetsuya.zama@gmail.com>
2017-03-03 15:10:47 +0900

    material-uiのインストール

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import Kyodoco from './component/kyodoco'
 import { Provider } from 'react-redux'
 import {configureStore} from './store/store'
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 import "babel-polyfill"
 
 const container = document.getElementById("app");
