79f4fb6fb091660093a6ab7990299ad5539337fa
tetsuya.zama <tetsuya.zama@gmail.com>
2017-04-16 17:37:41 +0900

    テストコードが出来たのでreducer/login.jsをリファクタリング

--- src/reducer/login.js
+++ src/reducer/login.js
@@ -1,6 +1,8 @@
 import {LOGIN_STATUS} from '../const/login'
 import {LOGIN_SUCCESS,LOGIN_FAILURE,LOGIN_REQUESTED,LOGOUT_REQUESTED} from '../action/login'
 
+const initialState = {status:LOGIN_STATUS.NOTYET,user:{}};
+
 /**
 * ログイン reducer
 * @see http://qiita.com/yasuhiro-okada-aktsk/items/9d9025cb58ffba35f864
@@ -8,16 +10,16 @@ import {LOGIN_SUCCESS,LOGIN_FAILURE,LOGIN_REQUESTED,LOGOUT_REQUESTED} from '../a
 * @param {Object} action dispatchされたaction
 * @return {Object} 変更後のstate
 */
-export default function login(state={status:"NOTYET",user:{}}, action){
+export default function login(state=initialState, action){
   switch(action.type){
     case LOGIN_SUCCESS:
       return {status:LOGIN_STATUS.SUCCESS,user:action.payload};
     case LOGIN_FAILURE:
       return {status:LOGIN_STATUS.FAILURE,user:{}};
     case LOGOUT_REQUESTED:
-      return {status:LOGIN_STATUS.NOTYET,user:{}};
+      return initialState;
     case LOGIN_REQUESTED:
-      return {status:LOGIN_STATUS.NOTYET,user:{}};
+      return initialState;
     default:
       return state;
   }
