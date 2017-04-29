d312fbba09abc2b96ff795077e85bfaae2f48701
tetsuya.zama <tetsuya.zama@gmail.com>
2017-04-29 15:34:54 +0900

    jsdomの設定ファイルに誤りがあったので修正

--- .jsdom.setup.js
+++ .jsdom.setup.js
@@ -1,8 +1,7 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-global.window = new JSDOM('');
-global.document = window.document;
+global.window = new JSDOM('').window;
 Object.keys(global.window).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     global[property] = window[property];
@@ -12,5 +11,3 @@ Object.keys(global.window).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js'
 };
-
-global.window.location = { "host": "localhost", "protocol": "http"};
