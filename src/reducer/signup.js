8a54820fe64e9f9e9547147b8d502940c868956d
tetsuya.zama <tetsuya.zama@gmail.com>
2017-05-01 06:40:36 +0900

    signup reducerのリファクタリング

--- src/reducer/signup.js
+++ src/reducer/signup.js
@@ -1,5 +1,7 @@
 import {SIGNUP_SUCCESS,SIGNUP_FAILURE} from '../action/signup'
 
+const initialState = {failure_reason:[]};
+
 /**
 * サインアップ reducer
 * @see http://qiita.com/yasuhiro-okada-aktsk/items/9d9025cb58ffba35f864
@@ -7,10 +9,10 @@ import {SIGNUP_SUCCESS,SIGNUP_FAILURE} from '../action/signup'
 * @param {Object} action dispatchされたaction
 * @return {Object} 変更後のstate
 */
-export default function signup(state={failure_reason:[]},action){
+export default function signup(state=initialState,action){
   switch(action.type){
     case(SIGNUP_SUCCESS):
-      return {failure_reason:[]};
+      return initialState;
     case(SIGNUP_FAILURE):
       return {failure_reason:action.payload};
     default:
