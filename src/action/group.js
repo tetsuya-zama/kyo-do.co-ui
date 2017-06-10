5549311af7d3e25282874571ad5353af405faab5
tetsuya.zama <tetsuya.zama@gmail.com>
2017-06-10 15:41:11 +0900

    group関連actionと単体テストコード

---
+++ src/action/group.js
@@ -0,0 +1,386 @@
+
+/**
+* ログインユーザーが所属しているグループをすべてロードするアクション名
+*/
+export const LOAD_USERS_GROUPS = "LOAD_USERS_GROUPS";
+
+/**
+* ログインユーザーが所属しているグループをすべてロードするアクションのcreator
+* @return {Object} LOAD_USERS_GROUPSアクション
+*/
+export function loadUsersGroups(){
+  return {
+    type:LOAD_USERS_GROUPS
+  };
+}
+
+/**
+* pollingによる所属グループロードアクション名
+*/
+export const LOAD_USERS_GROUPS_POLLING = "LOAD_USERS_GROUPS_POLLING";
+/**
+* pollingによる所属グループロードアクションのcreator
+* @return {Object} LOAD_USERS_GROUPS_POLLINGアクション
+*/
+export function loadUsersGroupsPolling(){
+  return {
+    type:LOAD_USERS_GROUPS_POLLING
+  };
+}
+
+/**
+* ユーザー所属グループのロード完了アクション名
+*/
+export const USERS_GROUPS_LOADED = "USERS_GROUPS_LOADED";
+
+/**
+* ユーザー所属グループのロード完了アクションのcreator
+* @param {Object} usersGroups ロードされたユーザー所属グループ
+* @return {Object} USERS_GROUPS_LOADEDアクション
+*/
+export function usersGroupsLoaded(usersGroups){
+  return {
+    type:USERS_GROUPS_LOADED,
+    payload:usersGroups
+  };
+}
+
+/**
+* グループ作成要求アクション名
+*/
+export const CREATE_GROUP_REQUIRED = "CREATE_GROUP_REQUIRED";
+
+/**
+* グループ作成要求アクションのcreator
+* @param {string} groupName 作成を要求するグループ名
+* @return {Object} CREATE_GROUP_REQUIREDアクション
+*/
+export function createGroupRequired(groupName){
+  return {
+    type: CREATE_GROUP_REQUIRED,
+    payload:{
+      groupName:groupName
+    }
+  };
+}
+
+/**
+* グループ作成成功アクション名
+*/
+export const CREATE_GROUP_SUCCESS = "CREATE_GROUP_SUCCESS";
+
+/**
+* グループ作成成功アクションのcreator
+* @return {Object} CREATE_GROUP_SUCCESSアクション
+*/
+export function createGroupSuccess(){
+  return {
+    type:CREATE_GROUP_SUCCESS
+  };
+}
+/**
+* グループ作成失敗アクション名
+*/
+export const CREATE_GROUP_FAILURE = "CREATE_GROUP_FAILURE";
+/**
+* グループ作成失敗アクションのcreator
+* @return {Object} CREATE_GROUP_FAILUREアクション
+*/
+export function createGroupFailure(){
+  return {
+    type: CREATE_GROUP_FAILURE
+  };
+}
+/**
+* グループへのメンバー追加要求アクション名
+*/
+export const ADD_MEMBER_TO_GROUP_REQUIRED = "ADD_MEMBER_TO_GROUP_REQUIRED";
+
+/**
+* グループへのメンバー追加要求アクションのcreator
+* @param {string} groupId 追加するグループのID
+* @param {string} memberId 追加するメンバーのID
+* @return {Object} ADD_MEMBER_TO_GROUP_REQUIREDアクション
+*/
+export function addMemberToGroupRequired(groupId,memberId){
+  return {
+    type:ADD_MEMBER_TO_GROUP_REQUIRED,
+    payload:{
+      groupId:groupId,
+      memberId:memberId
+    }
+  };
+}
+/**
+* グループへのメンバー追加成功アクション名
+*/
+export const ADD_MEMBER_TO_GROUP_SUCCESS = "ADD_MEMBER_TO_GROUP_SUCCESS";
+
+/**
+* グループへのメンバー追加成功アクションのcreator
+* @return {Object} ADD_MEMBER_TO_GROUP_SUCCESSアクション
+*/
+export function addMemberToGroupSuccess(){
+  return {
+    type: ADD_MEMBER_TO_GROUP_SUCCESS
+  };
+}
+/**
+* グループへのメンバー追加失敗アクション名
+*/
+export const ADD_MEMBER_TO_GROUP_FAILURE = "ADD_MEMBER_TO_GROUP_FAILURE";
+
+/**
+* グループへのメンバー追加失敗アクションのcreator
+* @return {Object} ADD_MEMBER_TO_GROUP_FAILUREアクション
+*/
+export function addMemberToGroupFailure(){
+  return {
+    type: ADD_MEMBER_TO_GROUP_FAILURE
+  };
+}
+/**
+* グループからメンバーの削除要求アクション名
+*/
+export const DELETE_MEMBER_FROM_GROUP_REQUIRED = "DELETE_MEMBER_FROM_GROUP_REQUIRED";
+
+/**
+* グループからメンバー削除要求アクションのcreator
+* @param {string} groupId メンバーを削除するグループのID
+* @param {string} memberId 削除されるメンバーのID
+* @return {Object} DELETE_MEMBER_FROM_GROUP_REQUIREDアクション
+*/
+export function deleteMemberFromGroupRequired(groupId,memberId){
+  return {
+    type:DELETE_MEMBER_FROM_GROUP_REQUIRED,
+    payload:{
+      groupId:groupId,
+      memberId:memberId
+    }
+  };
+}
+/**
+* グループからのメンバー削除成功アクション名
+*/
+export const DELETE_MEMBER_FROM_GROUP_SUCCESS = "DELETE_MEMBER_FROM_GROUP_SUCCESS";
+/**
+* グループからのメンバー削除成功アクションのcreator
+* @return DELETE_MEMBER_FROM_GROUP_SUCCESSアクション
+*/
+export function deleteMemberFromGroupSuccess(){
+  return {
+    type:DELETE_MEMBER_FROM_GROUP_SUCCESS
+  };
+}
+/**
+* グループからのメンバー削除失敗アクション名
+*/
+export const DELETE_MEMBER_FROM_GROUP_FAILURE = "DELETE_MEMBER_FROM_GROUP_FAILURE";
+
+/**
+* グループからのメンバー削除失敗アクションのcreator
+* @return DELETE_MEMBER_FROM_GROUP_FAILUREアクション
+*/
+export function deleteMemberFromGroupFailure(){
+  return {
+    type:DELETE_MEMBER_FROM_GROUP_FAILURE
+  }
+}
+/**
+* グループメンバーの管理者設定要求アクション名
+*/
+export const SET_MEMBER_AS_GROUP_ADMIN_REQUIRED = "SET_MEMBER_AS_GROUP_ADMIN_REQUIRED";
+
+/**
+* グループメンバーの管理者設定要求アクションのcreator
+* @param {string} groupId 管理者に設定するグループのID
+* @param {string} memberId 管理者に設定するメンバーのID
+* @return {Object} SET_MEMBER_AS_GROUP_ADMIN_REQUIREDアクション
+*/
+export function setMemberAsGroupAdminRequired(groupId,memberId){
+  return {
+    type:SET_MEMBER_AS_GROUP_ADMIN_REQUIRED,
+    payload:{
+      groupId:groupId,
+      memberId:memberId
+    }
+  };
+}
+
+/**
+* グループメンバーの管理者設定成功アクション名
+*/
+export const SET_MEMBER_AS_GROUP_ADMIN_SUCCESS = "SET_MEMBER_AS_GROUP_ADMIN_SUCCESS";
+
+/**
+* グループメンバーの管理者設定成功アクションのcreator
+* @return {Object} SET_MEMBER_AS_GROUP_ADMIN_SUCCESSアクション
+*/
+export function setMemberAsGroupAdminSuccess(){
+  return {
+    type:SET_MEMBER_AS_GROUP_ADMIN_SUCCESS
+  };
+}
+
+/**
+* グループメンバーの管理者設定失敗アクション名
+*/
+export const SET_MEMBER_AS_GROUP_ADMIN_FAILURE = "SET_MEMBER_AS_GROUP_ADMIN_FAILURE";
+
+/**
+* グループメンバーの管理者設定失敗アクションのcreator
+* @return {Object} SET_MEMBER_AS_GROUP_ADMIN_FAILUREアクション
+*/
+export function setMemberAsGroupAdminFailure(){
+  return {
+    type:SET_MEMBER_AS_GROUP_ADMIN_FAILURE
+  };
+}
+
+/**
+* グループメンバーの管理者設定解除アクション名
+*/
+export const UNSET_MEMBER_AS_GROUP_ADMIN_REQUIRED = "UNSET_MEMBER_AS_GROUP_ADMIN_REQUIRED";
+
+/**
+* グループメンバーの管理者設定解除アクションのcreator
+* @param {string} groupId　管理者設定を解除するグループのID
+* @param {string} memberId 管理者設定を解除するメンバーのID
+* @return {Object} UNSET_MEMBER_AS_GROUP_ADMIN_REQUIREDアクション
+*/
+export function unsetMemberAsGroupAdminRequired(groupId,memberId){
+  return {
+    type:UNSET_MEMBER_AS_GROUP_ADMIN_REQUIRED,
+    payload:{
+      groupId:groupId,
+      memberId:memberId
+    }
+  };
+}
+
+/**
+* グループメンバーの管理者設定解除成功アクション名
+*/
+export const UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESS = "UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESS";
+
+/**
+* グループメンバーの管理者設定解除成功アクションのcreator
+* @return {Object} UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESSアクション
+*/
+export function unsetMemberAsGroupAdminSuccess(){
+  return {
+    type:UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESS
+  };
+}
+
+/**
+* グループメンバーの管理者設定解除失敗アクション名
+*/
+export const UNSET_MEMBER_AS_GROUP_ADMIN_FAILURE = "UNSET_MEMBER_AS_GROUP_ADMIN_FAILURE";
+
+/**
+* グループメンバーの管理者設定解除失敗アクションのcreator
+* @return {Object} UNSET_MEMBER_AS_GROUP_ADMIN_FAILUREアクション
+*/
+export function unsetMemberAsGroupAdminFailure(){
+  return {
+    type:UNSET_MEMBER_AS_GROUP_ADMIN_FAILURE
+  };
+}
+
+/**
+* グループ名変更要求アクション名
+*/
+export const CHANGE_GROUP_NAME_REQUIRED = "CHANGE_GROUP_NAME_REQUIRED";
+
+/**
+* グループ名変更要求アクションのcreator
+* @param {string} groupId 名称を変更するグループのID
+* @param {string} newName 変更後の名称
+* @return {Object} CHANGE_GROUP_NAME_REQUIREDアクション
+*/
+export function changeGroupNameRequired(groupId,newName){
+  return {
+    type:CHANGE_GROUP_NAME_REQUIRED,
+    payload:{
+      groupId:groupId,
+      newName:newName
+    }
+  };
+}
+
+/**
+* グループ名変更成功アクション名
+*/
+export const CHANGE_GROUP_NAME_SUCCESS = "CHANGE_GROUP_NAME_SUCCESS";
+
+/**
+* グループ名変更成功アクションのcreator
+* @return {Object} CHANGE_GROUP_NAME_SUCCESSアクション
+*/
+export function changeGroupNameSuccess(){
+  return {
+    type:CHANGE_GROUP_NAME_SUCCESS
+  };
+}
+/**
+* グループ名変更失敗アクション名
+*/
+export const CHANGE_GROUP_NAME_FAILURE = "CHANGE_GROUP_NAME_FAILURE";
+
+/**
+* グループ名変更失敗アクションのcreator
+* @return {Object} CHANGE_GROUP_NAME_FAILUREアクション
+*/
+export function changeGroupNameFailure(){
+  return {
+    type:CHANGE_GROUP_NAME_FAILURE
+  };
+}
+
+/**
+*　グループ削除要求アクション名
+*/
+export const DELETE_GROUP_REQUIRED = "DELETE_GROUP_REQUIRED";
+
+/**
+* グループ削除要求アクションのcreator
+* @param {string} groupId 削除するグループのID
+* @return {Object} DELETE_GROUP_REQUIREDアクション
+*/
+export function deleteGroupRequired(groupId){
+  return {
+    type:DELETE_GROUP_REQUIRED,
+    payload:{
+      groupId:groupId
+    }
+  };
+}
+/**
+* グループ削除成功アクション名
+*/
+export const DELETE_GROUP_SUCCESS = "DELETE_GROUP_SUCCESS";
+
+/**
+* グループ削除成功アクションのcreator
+* @return DELETE_GROUP_SUCCESSアクション
+*/
+export function deleteGroupSuccess(){
+  return {
+    type:DELETE_GROUP_SUCCESS
+  };
+}
+
+/**
+* グループ削除失敗アクション名
+*/
+export const DELETE_GROUP_FAILURE = "DELETE_GROUP_FAILURE";
+/**
+* グループ削除失敗アクションのcreator
+* @return {Object} DELETE_GROUP_FAILUREアクション
+*/
+export function deleteGroupFailure(){
+  return {
+    type:DELETE_GROUP_FAILURE
+  };
+}
---
+++ test/action/group.test.js
@@ -0,0 +1,333 @@
+import assert from 'power-assert';
+import {
+  LOAD_USERS_GROUPS,
+  LOAD_USERS_GROUPS_POLLING,
+  USERS_GROUPS_LOADED,
+  CREATE_GROUP_REQUIRED,
+  CREATE_GROUP_SUCCESS,
+  CREATE_GROUP_FAILURE,
+  ADD_MEMBER_TO_GROUP_REQUIRED,
+  ADD_MEMBER_TO_GROUP_SUCCESS,
+  ADD_MEMBER_TO_GROUP_FAILURE,
+  DELETE_MEMBER_FROM_GROUP_REQUIRED,
+  DELETE_MEMBER_FROM_GROUP_SUCCESS,
+  DELETE_MEMBER_FROM_GROUP_FAILURE,
+  SET_MEMBER_AS_GROUP_ADMIN_REQUIRED,
+  SET_MEMBER_AS_GROUP_ADMIN_SUCCESS,
+  SET_MEMBER_AS_GROUP_ADMIN_FAILURE,
+  UNSET_MEMBER_AS_GROUP_ADMIN_REQUIRED,
+  UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESS,
+  UNSET_MEMBER_AS_GROUP_ADMIN_FAILURE,
+  CHANGE_GROUP_NAME_REQUIRED,
+  CHANGE_GROUP_NAME_SUCCESS,
+  CHANGE_GROUP_NAME_FAILURE,
+  DELETE_GROUP_REQUIRED,
+  DELETE_GROUP_SUCCESS,
+  DELETE_GROUP_FAILURE
+} from '../../src/action/group';
+
+import {
+  loadUsersGroups,
+  loadUsersGroupsPolling,
+  usersGroupsLoaded,
+  createGroupRequired,
+  createGroupSuccess,
+  createGroupFailure,
+  addMemberToGroupRequired,
+  addMemberToGroupSuccess,
+  addMemberToGroupFailure,
+  deleteMemberFromGroupRequired,
+  deleteMemberFromGroupSuccess,
+  deleteMemberFromGroupFailure,
+  setMemberAsGroupAdminRequired,
+  setMemberAsGroupAdminSuccess,
+  setMemberAsGroupAdminFailure,
+  unsetMemberAsGroupAdminRequired,
+  unsetMemberAsGroupAdminSuccess,
+  unsetMemberAsGroupAdminFailure,
+  changeGroupNameRequired,
+  changeGroupNameSuccess,
+  changeGroupNameFailure,
+  deleteGroupRequired,
+  deleteGroupSuccess,
+  deleteGroupFailure
+} from '../../src/action/group';
+/**@test {loadUsersGroups} */
+describe("loadUsersGroups action creator",()=>{
+  it("creates LOAD_USERS_GROUPS with no arguments",()=>{
+    const result = loadUsersGroups();
+
+    assert(result.type === LOAD_USERS_GROUPS);
+  })
+});
+/**@test {loadUsersGroupsPolling}*/
+describe("loadUsersGroupsPolling action creator",()=>{
+  it("creates LOAD_USERS_GROUPS_POLLING action with no arguments",()=>{
+    const result = loadUsersGroupsPolling();
+
+    assert(result.type === LOAD_USERS_GROUPS_POLLING);
+  })
+});
+/**@test {usersGroupsLoaded}*/
+describe("usersGroupsLoaded action creator",()=>{
+  it("creates USERS_GROUPS_LOADED action with array of group data which api returns",()=>{
+    const dummyAPIResult = [
+      {
+        id:"group_id_1",
+        name:"グループ1",
+        members:[
+          {
+            id:"member1",
+            name:"メンバー1",
+            isAdmin:true
+          },
+          {
+            id:"member2",
+            name:"メンバー2",
+            isAdmin:false
+          },
+        ]
+      },
+      {
+        id:"group_id_2",
+          name:"グループ2",
+        members:[
+          {
+            id:"member3",
+            name:"メンバー3",
+            isAdmin:true
+          },
+          {
+            id:"member3",
+            name:"メンバー3",
+            isAdmin:false
+          },
+        ]
+      }
+    ];
+
+    const result = usersGroupsLoaded(dummyAPIResult);
+
+    assert(result.type === USERS_GROUPS_LOADED);
+    assert.deepEqual(result.payload,dummyAPIResult);
+  });
+});
+/**@test {createGroupRequired} */
+describe("createGroupRequired action creator",()=>{
+  it("creates CREATE_GROUP_REQUIRED action with new group name",()=>{
+    const testGroupName = "test group";
+
+    const result = createGroupRequired(testGroupName);
+
+    assert(result.type === CREATE_GROUP_REQUIRED);
+    assert(result.payload.groupName === testGroupName);
+  });
+});
+
+/**@test {createGroupSuccess}*/
+describe("createGroupSuccess action creator",()=>{
+  it("creates CREATE_GROUP_SUCCESS action with no arguments",()=>{
+    const result = createGroupSuccess();
+
+    assert(result.type === CREATE_GROUP_SUCCESS);
+  });
+});
+
+/**@test {createGroupFailure}*/
+describe("createGroupFailure action creator",()=>{
+  it("creates CREATE_GROUP_FAILURE action with no arguments",()=>{
+    const result = createGroupFailure();
+
+    assert(result.type === CREATE_GROUP_FAILURE);
+  })
+});
+
+/**@test {addMemberToGroupRequired} */
+describe("addMemberToGroupRequired action creator",()=>{
+  it("creates ADD_MEMBER_TO_GROUP_REQUIRED action with target group ID and target member ID",()=>{
+    const targetGroupId = "group_id_1";
+    const targetMemberId = "member1";
+
+    const result = addMemberToGroupRequired(targetGroupId,targetMemberId);
+
+    assert(result.type === ADD_MEMBER_TO_GROUP_REQUIRED);
+    assert(result.payload.groupId === targetGroupId);
+    assert(result.payload.memberId === targetMemberId);
+  });
+});
+
+/**@test {addMemberToGroupSuccess} */
+describe("addMemberToGroupSuccess action creator",()=>{
+  it("creates ADD_MEMBER_TO_GROUP_SUCCESS action with no arguments",()=>{
+    const result = addMemberToGroupSuccess();
+
+    assert(result.type === ADD_MEMBER_TO_GROUP_SUCCESS);
+  });
+});
+
+/**@test {addMemberToGroupFailure}*/
+describe("addMemberToGroupFailure action creator",()=>{
+  it("creates ADD_MEMBER_TO_GROUP_FAILURE action with no arguments",()=>{
+    const result = addMemberToGroupFailure();
+
+    assert(result.type === ADD_MEMBER_TO_GROUP_FAILURE);
+  });
+});
+
+/**@test {deleteMemberFromGroupRequired} */
+describe("deleteMemberFromGroupRequired action creator",()=>{
+  it("creates DELETE_MEMBER_FROM_GROUP_REQUIRED action with target group ID and target member ID",()=>{
+    const targetGroupId = "group_id_1";
+    const targetMemberId = "member1";
+
+    const result = deleteMemberFromGroupRequired(targetGroupId,targetMemberId);
+
+    assert(result.type === DELETE_MEMBER_FROM_GROUP_REQUIRED);
+    assert(result.payload.groupId === targetGroupId);
+    assert(result.payload.memberId === targetMemberId);
+  });
+});
+
+/**@test {deleteMemberFromGroupSuccess} */
+describe("deleteMemberFromGroupSuccess action creator",()=>{
+  it("creates DELETE_MEMBER_FROM_GROUP_SUCCESS action with no arguments",()=>{
+    const result = deleteMemberFromGroupSuccess();
+
+    assert(result.type === DELETE_MEMBER_FROM_GROUP_SUCCESS);
+  });
+});
+
+/**@test {deleteMemberFromGroupFailure} */
+describe("deleteMemberFromGroupFailure action creator",()=>{
+  it("creates DELETE_MEMBER_FROM_GROUP_FAILURE action with no arguments",()=>{
+    const result = deleteMemberFromGroupFailure();
+
+    assert(result.type === DELETE_MEMBER_FROM_GROUP_FAILURE);
+  });
+});
+
+/**@test {setMemberAsGroupAdminRequired}*/
+describe("setMemberAsGroupAdminRequired action creator",()=>{
+  it("creates SET_MEMBER_AS_GROUP_ADMIN_REQUIRED action with target group ID and target member ID",()=>{
+    const targetGroupId = "group_id_1";
+    const targetMemberId = "member1";
+
+    const result = setMemberAsGroupAdminRequired(targetGroupId,targetMemberId);
+
+    assert(result.type === SET_MEMBER_AS_GROUP_ADMIN_REQUIRED);
+    assert(result.payload.groupId === targetGroupId);
+    assert(result.payload.memberId === targetMemberId);
+  });
+});
+
+/**@test {setMemberAsGroupAdminSuccess} */
+describe("setMemberAsGroupAdminSuccess action creator",()=>{
+  it("creates SET_MEMBER_AS_GROUP_ADMIN_SUCCESS action with no arguments",()=>{
+    const result = setMemberAsGroupAdminSuccess();
+
+    assert(result.type === SET_MEMBER_AS_GROUP_ADMIN_SUCCESS);
+  });
+});
+
+/**@test {setMemberAsGroupAdminFailure} */
+describe("setMemberAsGroupAdminFailure action creator",()=>{
+  it("creates SET_MEMBER_AS_GROUP_ADMIN_FAILURE action with no arguments",()=>{
+    const result = setMemberAsGroupAdminFailure();
+
+    assert(result.type === SET_MEMBER_AS_GROUP_ADMIN_FAILURE);
+  });
+});
+
+/**@test {unsetMemberAsGroupAdminRequired} */
+describe("unsetMemberAsGroupAdminRequired action creator",()=>{
+  it("creates UNSET_MEMBER_AS_GROUP_ADMIN_REQUIRED action with target group ID and target member ID",()=>{
+    const targetGroupId = "group_id_1";
+    const targetMemberId = "member1";
+
+    const result = unsetMemberAsGroupAdminRequired(targetGroupId,targetMemberId);
+
+    assert(result.type === UNSET_MEMBER_AS_GROUP_ADMIN_REQUIRED);
+    assert(result.payload.groupId === targetGroupId);
+    assert(result.payload.memberId === targetMemberId);
+  });
+});
+
+/**@test {unsetMemberAsGroupAdminSuccess} */
+describe("unsetMemberAsGroupAdminSuccess action creator",()=>{
+  it("creates UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESS action with no arguments",()=>{
+    const result = unsetMemberAsGroupAdminSuccess();
+
+    assert(result.type === UNSET_MEMBER_AS_GROUP_ADMIN_SUCCESS);
+  })
+});
+
+/**@test [unsetMemberAsGroupAdminFailure] */
+describe("unsetMemberAsGroupAdminFailure action creator",()=>{
+  it("creates UNSET_MEMBER_AS_GROUP_ADMIN_FAILURE action creator",()=>{
+    const result = unsetMemberAsGroupAdminFailure();
+
+    assert(result.type === UNSET_MEMBER_AS_GROUP_ADMIN_FAILURE);
+  });
+});
+
+/**@test {changeGroupNameRequired} */
+describe("changeGroupNameRequired action creator",()=>{
+  it("creates CHANGE_GROUP_NAME_REQUIRED action with targetGroupId and new group name",()=>{
+    const targetGroupId = "group_id_1";
+    const newGroupName = "new name";
+
+    const result = changeGroupNameRequired(targetGroupId,newGroupName);
+
+    assert(result.type === CHANGE_GROUP_NAME_REQUIRED);
+    assert(result.payload.groupId === targetGroupId);
+    assert(result.payload.newName === newGroupName);
+  });
+});
+
+/**@test {changeGroupNameSuccess} */
+describe("changeGroupNameSuccess action creator",()=>{
+  it("creates CHANGE_GROUP_NAME_SUCCESS action with no arguments",()=>{
+    const result = changeGroupNameSuccess();
+
+    assert(result.type === CHANGE_GROUP_NAME_SUCCESS);
+  });
+});
+
+/**@test {changeGroupNameFailure} */
+describe("changeGroupNameFailure action creator",()=>{
+  it("creates CHANGE_GROUP_NAME_FAILURE action with no arguments",()=>{
+    const result = changeGroupNameFailure();
+
+    assert(result.type === CHANGE_GROUP_NAME_FAILURE);
+  });
+});
+
+/**@test  {deleteGroupRequired} */
+describe("deleteGroupRequired action creator",()=>{
+  it("creates DELETE_GROUP_REQUIRED action with target group ID",()=>{
+    const targetGroupId = "group_id_1";
+
+    const result = deleteGroupRequired(targetGroupId);
+
+    assert(result.type === DELETE_GROUP_REQUIRED);
+    assert(result.payload.groupId === targetGroupId);
+  });
+});
+
+/**@test {deleteGroupSuccess} */
+describe("deleteGroupSuccess action creator",()=>{
+  it("creates DELETE_GROUP_SUCCESS action with no arguments",()=>{
+    const result = deleteGroupSuccess();
+
+    assert(result.type === DELETE_GROUP_SUCCESS);
+  });
+});
+
+/**@test {deleteGroupFailure} */
+describe("deleteGroupFailure action creator",()=>{
+  it("creates DELETE_GROUP_FAILURE action with no arguments",()=>{
+    const result = deleteGroupFailure();
+
+    assert(result.type === DELETE_GROUP_FAILURE);
+  })
+});
