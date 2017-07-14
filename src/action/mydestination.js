310747d4c364b76baeac4397ba8dca51da11e380
hiroshi-kimura-nst <hiroshi_kimura@nri-st.co.jp>
2017-07-14 11:22:36 +0900

    202
    action/mydistination.js
    【バグ？】自分の行先を編集する際、IEだと「×」が表示され、消去できるが、Safari、Chromeだと「×」が表示されない。

--- src/action/mydestination.js
+++ src/action/mydestination.js
@@ -33,3 +33,20 @@ export function myDestinationSaveComplete(destination){
     payload:destination
   };
 }
+
+/**
+* 自分の行き先クリアアクション
+*/
+export const MY_DESTINATION_CLEAR = "MY_DESTINATION_CLEAR";
+
+/**
+* 自分の行き先クリアアクションのcreator
+* @param {{inBusiness:boolean,comment:string,contact:string}} destination 行き先
+* @return {Object} 自分の行き先クリアアクション
+*/
+export function myDestinationClear(destination){
+  return {
+    type : MY_DESTINATION_CLEAR,
+    payload:destination
+  };
+}
